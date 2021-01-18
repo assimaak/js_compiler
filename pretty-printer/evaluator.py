9aaf3027de43d9c4c3e016a8a92081981a236724
root <root@ASUS-NORDINE.localdomain>
2021-01-18 14:20:50 +0100

    Ajout pour retranscrire un while

--- pretty-printer/evaluator.py
+++ pretty-printer/evaluator.py
@@ -13,12 +13,16 @@ class Evaluator:
             if x["type"]=="ExpressionStatement":
                 toAdd = str(self.evaluateExpression(x["expression"]))
                 #possible to avoid that if statement 
-                if toAdd[0] == "(":
-                    toAdd = toAdd[1:-1]
+                #if toAdd[0] == "(":
+                #    toAdd = toAdd[1:-1]
                 list_string.append((toAdd)+";")
             elif x["type"]=="VariableDeclaration":
                 toAdd=str(self.evaluateVariable(x["declarations"],0))
                 list_string.append(x["kind"]+" "+(toAdd)+";")
+            elif x["type"]=="WhileStatement":
+                block = Evaluator(x["body"]["body"])
+                toAdd="while "+str(self.evaluateExpression(x["test"])+" {\n\t"+('\n\t'.join(block.evaluateData()))+" \n}")
+                list_string.append(toAdd)
         return list_string
         
     
@@ -26,6 +30,17 @@ class Evaluator:
         result = ""
         if expr["type"] == "NumericLiteral":
             result = str(expr["value"])
+        elif expr["type"] == "Identifier":
+            result = expr["name"]
+        elif expr["type"] == "CallExpression":
+            args=[]
+            if not(expr["arguments"] is None):
+                for x in expr["arguments"]:
+                    args.append(self.evaluateExpression(x))
+            str_args = ", ".join(args)
+            result = expr["callee"]["name"]+"("+str_args+")"
+        elif expr["type"] == "UpdateExpression":
+            result = str(expr["argument"]["name"])+str(expr["operator"])
         elif expr["type"] == "BinaryExpression":
             result = "("+str(self.evaluateExpression(expr["left"]))+str(expr["operator"])+str(self.evaluateExpression(expr["right"]))+")"
         return result
